34ba5474625f2b9486d5a5967fcae2cc9816fb87
Pasindu Ransara <140839432+PasinduRansara10@users.noreply.github.com>
2023-07-31 20:50:06 +0530

    Add files via upload

---
+++ Lab_2023.06.19/Q1/Student.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.student;
+
+/**
+ *
+ * @author Pasindu
+ */
+public class Student{
+
+    final int marks = 100;
+    //mark=90;
+    //final variable cannot be re-assigned
+    final void display(){
+        System.out.println(marks);
+    }
+}
